a477c0f857f08d8913bbaa40746ab03a284382cf
rezvanabedin <rezvanabedin79@gmail.com>
2021-04-06 06:52:09 -0700

    adding player2

--- main.cpp
+++ main.cpp
@@ -4986,3 +4986,35 @@ void TicTacToe::setP2G3(int data, vector<int>& p1, vector<int>& p2) {
     if (data >= 1 && data <= 21 && flag == 0) {
         p2.push_back(data);
 }
+
+/*rezvan2|player2*/
+#include<iostream>
+#include<boost/asio.hpp>
+#include<thread>
+#include<vector>
+#include<string>
+
+using namespace std;
+using namespace boost::asio;
+using namespace ip;
+
+string help();
+
+int main()
+{
+	io_service io2;
+	tcp::socket sock(io2);
+	sock.connect(tcp::endpoint(address::from_string("127.0.0.1"), 1234));
+	while (1)
+	{
+		boost::asio::streambuf buff2;
+		read_until(sock, buff2, "\n");
+		cout << buffer_cast <const char*> (buff2.data());
+
+		string msg2;
+		cout << "it's your turn: ";
+		getline(cin, msg2);
+		msg2 += "\n";
+		write(sock, boost::asio::buffer(msg2));
+	}
+}
